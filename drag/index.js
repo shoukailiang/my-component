80d207cce49e8ab4fad94753e35c3b71554dfee8
shoukailiang <shoukailiang@qq.com>
2017-12-09 20:47:48 +0800

    use es6 and add a changesth function to change style,fixed some bug

--- drag/index.js
+++ drag/index.js
@@ -1,37 +1,65 @@
 window.onload = function () {
-  var div1 = new Drag('box1')
-  div1.init();
+  var div1 = new Drag('box1');
 }
-function Drag(id) {
-  this.disX = 0;
-  this.disY = 0;
-  this.oDiv = document.getElementById(id);
-}
+class Drag {
+  constructor(id) {
+    this.disX = 0;
+    this.disY = 0;
+    this.oDiv = document.getElementById(id);
+    this.init()
+  }
+  init() {
+    var _this = this;
+    this.oDiv.onmousedown = function (ev) {
+      var ev = ev || window.event;
+      /* 传入_this,为了下面不在重复写 */
+      _this.fnDown(ev, _this)
+    };
+    /* 默认 */
+    this.changeSth({
+      'width': '200px',
+      'height': '200px',
+      'backgroundColor': 'red',
+      'position': 'absolute'
+    })
+  }
+  /* 拖拽本体 */
+  fnDown(ev, _this) {
+    this.disX = ev.clientX - this.oDiv.offsetLeft;
+    this.disY = ev.clientY - this.oDiv.offsetTop;
+    document.onmousemove = function (ev) {
+      _this.fnMove(ev);
+    }
+    document.onmouseup = this.fnUp;
+    /* 阻止默认事件 */
+    return false;
+  }
+  fnMove(ev) {
+    this.oDiv.style.left = ev.clientX - this.disX + 'px';
+    this.oDiv.style.top = ev.clientY - this.disY + 'px';
 
-Drag.prototype.init = function () {
-  var _this = this;
-  document.onmousedown = function () {
-    _this.fnDown();
+  }
+  fnUp() {
+    document.onmousemove = null;
+    document.onmouseup = null;
+  }
+  /* 改变一些样式 */
+  changeSth(json) {
+    for (var attr in json) {
+      /* 判断是否css样式表中已经定义了css */
+      if (json[attr] != getStyle(this.oDiv, attr)) {
+        this.oDiv.style[attr] = json[attr]
+      }
+    }
+  }
+}
+/*获取最终的css */
+function getStyle(obj, attr) {
+  if (obj.currentStyle) {
+    return obj.currentStyle[attr];
+  }
+  else {
+    return getComputedStyle(obj, false)[attr];
   }
 }
 
-Drag.prototype.fnDown = function () {
-  var _this = this;
-  var ev = ev || window.event;
-  this.disX = ev.clientX - this.oDiv.offsetLeft;
-  this.disY = ev.clientY - this.oDiv.offsetTop;
-  document.onmousemove = function () {
-    _this.fnMove();
-  }
-  document.onmouseup =this.fnUp
-  return false;
-}
-Drag.prototype.fnMove = function (ev) {
-  var ev = ev || window.event;
-  this.oDiv.style.left = ev.clientX - this.disX + 'px';
-  this.oDiv.style.top = ev.clientY - this.disY + 'px';
-}
-Drag.prototype.fnUp = function () {
-  document.onmousemove = null;
-  document.onmouseup = null;
-}
